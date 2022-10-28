7d36879e63860c56ac1d59799336c46c7fb9aad5
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-10-28 12:36:15 -0700

    edit configure services method in startup.cs to support authentication

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,17 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+// using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc;
+// using ApiVersioningSampleApp.Controllers; //Using to access controllers
+using Microsoft.OpenApi.Models; //Using for Swagger OpenAPI
+using System; //Using for Uri
+using System.Reflection; //Using for XML comments with swagger
+using System.IO; //Using for XML comments with swagger
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
 using ParksLookup.Models;
 
 namespace ParksLookup
@@ -24,6 +35,27 @@ namespace ParksLookup
             services.AddDbContext<ParksLookupContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
+            services.Configure<JwtConfig>(Configuration.GetSection("JwtConfig"));
+
+                        // within this section we are configuring the authentication and setting the default scheme
+            services.AddAuthentication(options => {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(jwt => {
+                var key = Encoding.ASCII    .GetBytes(Configuration["JwtConfig:Secret"]);
+
+                jwt.SaveToken = true;
+                jwt.TokenValidationParameters = new TokenValidationParameters{
+                    ValidateIssuerSigningKey= true, // this will validate the 3rd part of the jwt token using the secret that we added in the appsettings and verify we have generated the jwt token
+                    IssuerSigningKey = new SymmetricSecurityKey(key), // Add the secret key to our Jwt encryption
+                    ValidateIssuer = false, 
+                    ValidateAudience = false,
+                    RequireExpirationTime = false,
+                    ValidateLifetime = true
+                }; 
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,7 +70,7 @@ namespace ParksLookup
 
             app.UseRouting();
 
-            // app.UseAuthorization();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
