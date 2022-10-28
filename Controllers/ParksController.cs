00973e5fa719143010d489319d8e6dfddc39a0ba
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-10-28 09:31:53 -0700

    initial commit

---
+++ Controllers/ParksController.cs
@@ -0,0 +1,18 @@
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.Extensions.Logging;
+
+// [Route("api/[controller]")]
+//   [ApiController]
+//   public class ParksController : ControllerBase
+//   {
+//     private readonly ParksLookupContext _db;
+
+//     public ParksController(ParksLookupContext db)
+//     {
+//       _db = db;
+//     }
+//   }
---
+++ Models/Park.cs
@@ -0,0 +1,11 @@
+namespace ParksLookup.Models
+{
+    public class Park
+    {
+        public int ParkId { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string Type { get; set; }
+    }
+}
